b89ce1b34a3f15670e91d02b5491d58163251ff7
vozzhaevsn <Vip.yaaaz98@gmail.com>
2024-10-29 23:00:08 +1000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,6 +2,11 @@ import pytest
 import sqlite3
 from unittest.mock import patch
 from customers_modules import view_events, register_customer, cancel_ticket
+from admin_modules import (
+    get_current_events, add_event, delete_event, get_all_customers,
+    get_customers_for_event, get_customers_not_for_event,
+    get_events_ranked_by_tickets, get_events_ranked_by_revenue
+)
 
 @pytest.fixture(scope='function')
 def db_connection():
@@ -52,23 +57,17 @@ def test_view_events_no_events(db_connection):
         mock_print.assert_called_with("Нет доступных спектаклей.")
 
 def test_view_events_with_events(db_connection):
-    
-    """Тест для функции view_events с доступными спектаклями."""
-    cursor = db_connection.cursor()
-    cursor.execute("INSERT INTO events (name, price, date, free_slots) VALUES (?, ?, ?, ?)",
-                   ("Test Event", 100.0, "2023-10-01", 5))
-    db_connection.commit()
+    add_event("Test Event", 100.0, "2024-11-01", 5)
 
-    with patch('builtins.input', side_effect=["1"]), patch('builtins.print') as mock_print:
-        view_events()
-        mock_print.assert_any_call("Доступные спектакли:")
-        mock_print.assert_any_call("ID: 1, Название: Test Event, Цена: 100.0, Дата: 2023-10-01, Свободные места: 5")
+    with patch('builtins.print') as mocked_print:
+        view_events()  # Вызываем функцию
+        mocked_print.assert_any_call('ID: 1, Название: Test Event, Цена: 100.0, Дата: 2024-11-01, Свободные места: 5')
 
 def test_register_customer(db_connection):
     """Тест для функции register_customer."""
     cursor = db_connection.cursor()
     cursor.execute("INSERT INTO events (name, price, date, free_slots) VALUES (?, ?, ?, ?)",
-                   ("Test Event", 100.0, "2023-10-01", 5))
+                   ("Test Event", 100.0, "2024-11-01", 5))
     db_connection.commit()
 
     with patch('builtins.input', side_effect=["John Doe", 30, "john.doe@example.com", "1234567890"]), patch('builtins.print') as mock_print:
@@ -82,22 +81,12 @@ def test_cancel_ticket_no_order(db_connection):
         mock_print.assert_called_with("Заказ с таким номером не найден.")
 
 def test_cancel_ticket_with_order(db_connection):
-    """Тест для функции cancel_ticket с существующим заказом."""
+    add_event("Test Event", 100.0, "2023-10-01", 5)
     cursor = db_connection.cursor()
-    cursor.execute("INSERT INTO events (name, price, date, free_slots) VALUES (?, ?, ?, ?)",
-                   ("Test Event", 100.0, "2023-10-01", 5))
-    cursor.execute("INSERT INTO customers (name, age, email, phone) VALUES (?, ?, ?, ?)",
-                   ("John Doe", 30, "john.doe@example.com", "1234567890"))
-    
-    cursor.execute('''
-        INSERT INTO orders (customer_id, event_id, event_name, event_price)
-        VALUES (?, ?, ?, ?)
-    ''', (1, 1, "Test Event", 100.0))
+    cursor.execute("INSERT INTO customers (name, age, email, phone) VALUES (?, ?, ?, ?)", ("John Doe", 30, "john@example.com", "1234567890"))
+    cursor.execute("INSERT INTO orders (customer_id, event_id) VALUES (?, ?)", (1, 1))
     db_connection.commit()
+    
+    result = cancel_ticket(1)  # Указываем ID заказа для отмены
+    assert result == 1  # Ожидаем успешное удаление
 
-    with patch('builtins.input', side_effect=["1"]), patch('builtins.print') as mock_print:
-        cancel_ticket()
-        cursor.execute("SELECT COUNT(*) FROM orders WHERE order_id = 1")
-        count = cursor.fetchone()[0]
-        assert count == 0  # Заказ должен быть удален
-        mock_print.assert_called_with("Заказ успешно отменен.")
